a41e4006fc66eee020cf9805610ef28c72767936
jojoarman1 <138059765+jojoarman1@users.noreply.github.com>
2024-03-31 21:09:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -235,20 +235,11 @@ def close_menu_handler(call):
     bot.delete_message(call.message.chat.id, call.message.message_id)
 
 
-
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-# Остальная часть вашего кода остается без изменений
-
-def main():
-    while True:
-        try:
-            bot.polling(none_stop=True, interval=0, timeout=20)
-        except Exception as e:
-            logging.exception("Возникла ошибка во время опроса бота: %s", e)
-            time.sleep(15)
-
-if __name__ == 'main':
-    main()
+    bot.polling(none_stop=True)  # Запускаем опрос бота
+except Exception as e:
+    logging.error("Возникло исключение во время опроса:", e)
+    time.sleep(15)  # Делаем паузу перед повторной попыткой
+else:
+    logging.info("Опрос бота завершен успешно.")
+finally:
+    logging.info("Бот остановлен.")
